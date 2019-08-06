e2aed5cad27127b44b476b4f23639d2c0ff41aec
gphirke <50782307+gphirke@users.noreply.github.com>
2019-08-06 11:06:31 +0530

    promise example

---
+++ promise.js
@@ -0,0 +1,23 @@
+// this function, waitThreeSeconds, returns a promise
+function waitThreeSeconds() {
+  // below is the constructor for the promise. It takes a 
+  // callback that accepts two arguments,
+  // resolve and reject (functions)
+  // you call these functions when the code succeeds or fails
+  return new Promise(function(resolve, reject) {
+   setTimeout(function() {
+      // resolve the promise after 3 seconds
+      // the function you pass to 'then' will be called
+      resolve();
+    }, 3000);
+  })
+}
+// call the asynchronous function, which returns our promise
+waitThreeSeconds()
+  // chain the 'then' method, which accepts a callback to 
+  // be called when the promise is rsolved
+ .then(function() {
+   alert('It\'s been three seconds!');
+ })
+ // we don't need a catch method in this example, because the
+ // promise has no potential to fail (be rejected)
