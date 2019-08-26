f31d9bd17ab7b8a193e3fb36a0fdc2bb3f8f5d94
gphirke <50782307+gphirke@users.noreply.github.com>
2019-08-27 00:00:53 +0530

    comment added

--- group-by-key.js
+++ group-by-key.js
@@ -15,6 +15,32 @@ var obj = [{
            experience: "3 years"
        }];
 
+//o/p should be like 
+//
+   {
+       "Developer": [
+           {
+               employeeId: "2001",
+               designation: "Developer",
+               experience: "4 years"
+           },
+           {
+               employeeId: "2003,
+               designation: "Developer",
+               experience: "3 years"
+           }
+       ],
+       "QA": [
+           {
+               employeeId: "2002",
+               designation: "QA",
+               experience: "3 years"
+           }
+       ]
+   }
+
+//**********solution**********//
+
 function groupBy(designation) {
    var tempObj = {};
    obj.map(function(ele){
