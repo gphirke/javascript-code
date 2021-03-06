8569c1bf767aec76f7a3986b29c45c944b3ab0d0
gphirke <50782307+gphirke@users.noreply.github.com>
2021-03-06 12:18:43 +0530

    Create moveElement.js

---
+++ moveElement.js
@@ -0,0 +1,12 @@
+// Create a function to move an element. The function arguments are,
+// distance, duration, and the element to move.
+
+function moveElement(duration, distance, element) {
+  var start = performance.now();
+    function move(){
+            
+    }
+  
+  window.requestAnimationFrame(move);
+
+}
