1387824949eb5c4cea64347d883f15cb0d809807
gphirke <50782307+gphirke@users.noreply.github.com>
2021-03-06 15:02:38 +0530

    Update moveElement.js

--- moveElement.js
+++ moveElement.js
@@ -1,12 +1,24 @@
 // Create a function to move an element. The function arguments are,
 // distance, duration, and the element to move.
+// ref  https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+// https://github.com/young/frontend-interviewing/blob/master/questions/moveElement.js
 
-function moveElement(duration, distance, element) {
-  var start = performance.now();
-    function move(){
-            
-    }
-  
-  window.requestAnimationFrame(move);
 
+
+function moveElement(duration, distance, element){
+	var start = performance.now();
+	function move(currentTime){
+		var elapsed = currentTime - start;
+		var progress = elapsed/duration;
+		var amountToMove = progress*distance;
+		element.style.transform = `translateX(${amountToMove}px)`;
+
+		if (amountToMove < distance) {
+			window.requestAnimationFrame(move);
+		}
+
+	}
+	window.requestAnimationFrame(move);
 }
+
+moveElement(1000, 500, document.getElementById("element"));
