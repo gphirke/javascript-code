ee11cf75c96bb43a0e0ab4bfb543da797ddf1691
gphirke <50782307+gphirke@users.noreply.github.com>
2021-03-15 13:35:57 +0530

    Create exercise4.js

---
+++ exercise4.js
@@ -0,0 +1,7 @@
+//holy-grail layout 
+//reference https://philipwalton.github.io/solved-by-flexbox/demos/holy-grail/
+
+//example https://codepen.io/mynoralexander/pen/OVGLmo
+//https://codepen.io/thedigitalman/pen/rAHCj
+//https://codepen.io/catheraaine/pen/vGemrK
+//https://codepen.io/geoffgraham/pen/yPYKzw
