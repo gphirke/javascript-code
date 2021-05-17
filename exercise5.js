ef28183e59d4eaf05fe565bff263f5d622b76b83
gphirke <50782307+gphirke@users.noreply.github.com>
2021-05-17 10:55:53 +0530

    Create exercise5.js

---
+++ exercise5.js
@@ -0,0 +1,12 @@
+Return an array   all the primitive values from a given JSON 
+Eg Input -  {
+    name: 'John',
+    details: {
+        id: 308,
+        address: {
+            city: 'Bangalore'
+        }
+    }
+}
+
+Expected output - [‘John’, 308, ‘Bangalore’]
